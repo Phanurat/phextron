f3379cc205b8d902669e6f07e40c529eb1684bcc
Phanurat <paunavt5@gmail.com>
2025-05-12 18:41:31 +0700

    setup project

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const connectBtn = document.getElementById('connectBtn');
+
+connectBtn.onclick = async () => {
+  if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      connectBtn.innerText = `Connected: ${accounts[0].slice(0, 6)}...`;
+    } catch (err) {
+      alert('Connection failed.');
+    }
+  } else {
+    alert('Install MetaMask to use this app.');
+  }
+};
