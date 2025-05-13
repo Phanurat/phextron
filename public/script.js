82b7ea8a85138ea688f18c98eb9980d2f6abcd93
Phanurat <paunavt5@gmail.com>
2025-05-13 19:27:11 +0700

    add swap feature

--- public/script.js
+++ public/script.js
@@ -2,14 +2,42 @@ const connectBtn = document.getElementById('connectBtn');
 const menu = document.getElementById('menu');
 
 connectBtn.onclick = async () => {
-  if (window.ethereum) {
-    try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      connectBtn.innerText = `Connected: ${accounts[0].slice(0, 6)}...`;
-    } catch (err) {
-      alert('Connection failed.');
+    if (window.ethereum) {
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            connectBtn.innerText = `Connected: ${accounts[0].slice(0, 6)}...`;
+        } catch (err) {
+            alert('Connection failed.');
+        }
+    } else {
+        alert('Install MetaMask to use this app.');
     }
-  } else {
-    alert('Install MetaMask to use this app.');
-  }
 };
+
+const fromIcon = document.getElementById("fromIcon");
+const toIcon = document.getElementById("toIcon");
+
+function updateIcons() {
+    const fromSelected = fromToken.options[fromToken.selectedIndex];
+    const toSelected = toToken.options[toToken.selectedIndex];
+
+    const fromIconSrc = fromSelected.getAttribute("data-icon");
+    const toIconSrc = toSelected.getAttribute("data-icon");
+
+    fromIcon.src = fromIconSrc;
+    toIcon.src = toIconSrc;
+}
+
+fromToken.addEventListener("change", () => {
+    updateRate();
+    updateIcons();
+});
+
+toToken.addEventListener("change", () => {
+    updateRate();
+    updateIcons();
+});
+
+// เรียกตอนโหลดหน้า
+updateRate();
+updateIcons();
